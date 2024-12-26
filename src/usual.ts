103c8173756d2574bcae91043a38ab3ab1d5c027
Xli33 <lshin1232@gmail.com>
2024-12-26 14:24:11 +0800

    feat: 新增setPathValue，scroller与toTopOrBottom
    setPathValue：通过给定路径为传入对象设置值
    scroller：滚动元素内容至指定位置
    toTopOrBottom：滚动元素内容至顶部或底部
    
    BREAKING CHANGE: 原backToTop改为toTopOrBottom

--- src/usual.ts
+++ src/usual.ts
@@ -77,39 +77,185 @@ export function getPathValue(obj: Obj, keyPath: string, check?: boolean): any {
 }
 
 /**
- * 变速滚动元素内容至顶部
- * @param selector css selector
- * @param step the bigger, the faster
- * @param cb callback after scrolling done
+ * 通过给定键路径为传入对象设置值
+ * @param obj 需要通过keyPath设置value的对象
+ * @param keyPath 以.分隔的键路径，如 a.b.c
+ * @param value 设置的值
+ * @returns 赋值成功则返回 true
+ * @example const a = { one: { two: [ 3, {} ] } }
+ * setPathValue(a, 'one.two.1.four', 1) === true
+ * a.one.two[1].four === 1
  */
-export const backToTop = (selector = 'html', step = 5, cb?: () => void) => {
-  const dom = document.querySelector(selector);
-  if (!dom) return;
-  const scroll = () => {
-    // console.log(dom.scrollTop);
-    dom.scrollTop -= dom.scrollTop / 10 + step * (dom.scrollTop / 100);
-    dom.scrollTop > 0 ? window.requestAnimationFrame(scroll) : cb && cb();
-  };
-  scroll();
+export function setPathValue(obj: Obj, keyPath: string, value?: any) {
+  if (!obj || typeof obj !== 'object') {
+    console.warn('obj is not an object');
+    return;
+  }
+  let len,
+    curr = obj;
+  const arr = (keyPath || '')
+    .split('.')
+    .map((e) => e.trim())
+    .filter((e) => !!e);
+  if ((len = arr.length) < 0) return;
+  for (let i = 0; i < len; i++) {
+    // 进入循环，说明arr必然是包含非空key的数组，所以正常取到最终目标后，循环正好结束
+    // 若curr不是对象，则不能赋值，应直接return
+    // eg. 如 setPathValue({a: { b: 0 } }, 'a.b.c', 1)，传入对象的 a.b 是 0，不存在属性 c，故无法对其设置值（在严格模式下对原始值设置属性会报错）
+    if (curr == null || typeof curr !== 'object') return;
+    // 若循环至最后一个key，则进行赋值，否则继续循环
+    if (i < len - 1) {
+      curr = curr[arr[i]];
+    } else {
+      curr[arr[i]] = value;
+    }
+  }
+  return true;
+}
+
+/**
+ * @example 
+			animate(
+				500,
+				(progress) => {
+					box.style.transform =
+						"translateX(" + progress * 300 + "px)";
+				},
+				easeOut,
+			);
+ */
+// function animate(duration: number, draw: (num: number) => void, timing: (num: number) => number) {
+//   const start = performance.now();
+//   requestAnimationFrame(function loop(time) {
+//     let timeFraction = (time - start) / duration;
+//     if (timeFraction > 1) timeFraction = 1;
+//     const progress = timing(timeFraction); // 使用缓动函数
+//     draw(progress);
+//     timeFraction < 1 && requestAnimationFrame(loop);
+//   });
+// }
+
+const transit = {
+  linear: (timeFraction: number) => timeFraction,
+  easeOut: (timeFraction: number) => 1 - Math.pow(1 - timeFraction, 3),
+  easeIn: (timeFraction: number) => Math.pow(timeFraction, 3),
+  easeInOut: (timeFraction: number) =>
+    timeFraction < 0.5 ? 4 * Math.pow(timeFraction, 3) : 1 - Math.pow(-2 * timeFraction + 2, 3) / 2
 };
 
-// todo
-// export const scrollTo = (
-//   el = document.documentElement,
-//   top: number,
-//   left: number,
-//   // speed = 5,
-//   // cb?: () => void
-// ) => {
-//   if (el.scroll) {
-//     el.scroll({
-//       top,
-//       left,
-//       behavior: 'smooth'
-//     });
-//     return;
+// function transit(type: 'linear' | 'easeIn' | 'easeOut' | 'easeInOut' = 'linear', timeFraction: number) {
+//   if(type === 'easeIn') {
+//     return 1 - Math.pow(1 - timeFraction, 3)
 //   }
-// };
+//   if(type === 'easeOut') {
+//     return Math.pow(timeFraction, 3)
+//   }
+//   if(type === 'easeInOut') {
+//     // 	timeFraction < 0.5
+//     // 		? 2 * timeFraction * timeFraction
+//     // 		: -1 + (4 - 2 * timeFraction) * timeFraction;
+//     return timeFraction < 0.5 ? 4 * Math.pow(timeFraction, 3) : 1 - Math.pow(-2 * timeFraction + 2, 3) / 2
+//   }
+//   return 1 - Math.pow(1 - timeFraction, 3)
+// }
+
+type timingTypes = 'linear' | 'easeIn' | 'easeOut' | 'easeInOut';
+/**
+ * 滚动元素内容至指定位置。未指定有效duration与type时尝试调用原生scroll({behavior:'smooth'})
+ * @param el html element，未指定则使用根元素：html
+ * @param duration 过渡持续时间，单位ms
+ * @param top 滚动到指定的scrollTop
+ * @param left 滚动到指定的scrollLeft
+ * @param type 过渡曲线
+ * @example
+ * scroller({
+ *  top: 0,
+ *  // duration: 500,
+ *  // type: 'easeOut'
+ * })
+ */
+export function scroller({
+  el,
+  duration,
+  top,
+  left,
+  type
+}: {
+  el?: Element;
+  duration?: number;
+  top?: number;
+  left?: number;
+  type?: timingTypes;
+}) {
+  if (!el) el = document.documentElement;
+  if (el.scroll && !type && duration > 0) {
+    el.scroll({
+      top,
+      left,
+      behavior: 'smooth'
+    });
+    return;
+  }
+  const begin = performance.now(),
+    curve = transit[type];
+  let fromTop: number,
+    fromLeft: number,
+    disTop: number,
+    disLeft: number,
+    validTop: boolean,
+    validLeft: boolean;
+  if (typeof top === 'number') {
+    fromTop = el.scrollTop;
+    disTop = top - fromTop;
+    validTop = disTop !== 0;
+  }
+  if (typeof left === 'number') {
+    fromLeft = el.scrollLeft;
+    disLeft = left - fromLeft;
+    validLeft = disLeft !== 0;
+  }
+  requestAnimationFrame(function loop(now: number) {
+    const timeElapsed = now - begin,
+      progress = curve(Math.min(timeElapsed / duration, 1));
+    if (validTop) {
+      el.scrollTop = fromTop + disTop * progress;
+    }
+    if (validLeft) {
+      el.scrollLeft = fromLeft + disLeft * progress;
+    }
+    timeElapsed < duration && requestAnimationFrame(loop);
+  });
+  // requestAnimationFrame(animateScroll);
+}
+
+/**
+ * 滚动元素内容至顶部或底部，未指定duration与type则尝试调用原生scroll({behavior:'smooth'})
+ * @param el html element，未指定则使用根元素：html
+ * @param dir 'top' | 'bottom'
+ * @param duration 过渡时间，单位ms
+ * @param type 过渡曲线
+ * @example toTopOrBottom()
+ * toTopOrBottom(null, 'bottom')
+ */
+export function toTopOrBottom(
+  el?: Element,
+  dir: 'top' | 'bottom' = 'top',
+  duration: number = 500,
+  type?: timingTypes
+) {
+  scroller({
+    el,
+    duration,
+    top: dir === 'top' ? 0 : el.scrollHeight - el.clientHeight,
+    type
+  });
+  // const scroll = () => {
+  //   // console.log(dom.scrollTop);
+  //   dom.scrollTop -= dom.scrollTop / 10 + step * (dom.scrollTop / 100);
+  //   dom.scrollTop > 0 ? window.requestAnimationFrame(scroll) : cb && cb();
+  // };
+  // scroll();
+}
 
 /**
  * 复制到剪贴板
