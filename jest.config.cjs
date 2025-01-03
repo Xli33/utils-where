7263da60379aad57ae71d59a32b461d767355746
Xli33 <lshin1232@gmail.com>
2025-01-03 22:23:55 +0800

    build: 调整test配置

--- jest.config.cjs
+++ jest.config.cjs
@@ -2,7 +2,15 @@ module.exports = {
   preset: 'ts-jest',
   // testEnvironment: 'node',
   transform: {
-    '^.+\\.ts?$': 'ts-jest'
+    // '^.+\\.ts?$': 'ts-jest'
+    // '^.+\\.[tj]sx?$' to process ts,js,tsx,jsx with `ts-jest`
+    // '^.+\\.m?[tj]sx?$' to process ts,js,tsx,jsx,mts,mjs,mtsx,mjsx with `ts-jest`
+    '^.+\\.ts?$': [
+      'ts-jest',
+      {
+        // tsconfig: 'tsconfig.test.json'
+      }
+    ]
   }
   // transformIgnorePatterns: ['/node_modules/', '\\.pnp\\.[^\\/]+$']
 };
