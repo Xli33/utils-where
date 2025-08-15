746ec88b97527cb735e43b3313baf89ad1741158
Xli33 <lshin1232@gmail.com>
2025-08-15 17:24:27 +0800

    feat: unusual模块新增delArrItemByVal函数，用于从给定数组中根据值移除指定项

--- src/unusual.ts
+++ src/unusual.ts
@@ -122,8 +122,6 @@ export function deepMerge(
  * @returns 包含被删除项的数组
  * @example delArrItem([null, 5, 'as', {}, false], [3,1,7]) => [5, {}]
  */
-// export function delArrItem(arr: any[], indexes: number[]): any[];
-// export function delArrItem(arr: any, indexes: any): void;
 export function delArrItem(arr: any[], indexes: number[]) {
   if (!Array.isArray(arr) || !Array.isArray(indexes)) return [];
   const len = arr.length,
@@ -132,6 +130,24 @@ export function delArrItem(arr: any[], indexes: number[]) {
     res.unshift(arr.splice(e, 1)[0]);
   });
   return res;
+}
+
+/**
+ * 从arr中删除items里的元素
+ * @param arr 需要删除指定项的数组
+ * @param items 要从arr中移除掉的项
+ * @returns 删除了给定项的源数组arr
+ * @example delArrItemByVal([2, '', alert, console, false, NaN], ['', alert, console, NaN]) => [2, false]
+ */
+export function delArrItemByVal(arr: any[], items: any[]) {
+  if (!Array.isArray(arr) || !Array.isArray(items)) return arr;
+  let index;
+  for (const v of items) {
+    // 唯一与自身不等的只有NaN，indexOf使用严格相等（与 === 运算符使用的算法相同），故indexOf(NaN)结果是 -1。此处通过findIndex单独查找NaN的索引
+    index = !Number.isNaN(v) ? arr.indexOf(v) : arr.findIndex((e) => Number.isNaN(e) /* e !== e */);
+    index > -1 && arr.splice(index, 1);
+  }
+  return arr;
 }
 
 /**
