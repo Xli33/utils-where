0b2ae2c466613bbfb3c080f4329921ab2ac985fa
Xli33 <lshin1232@gmail.com>
2025-08-16 16:07:35 +0800

    fix: 优化events.ts，完善longpress事件监听&移除逻辑
    修复连续触发touchstart，且在_longPressDelay之内触发touchend后仍可能触发longpress事件
    移除longpress事件时同时对比监听时的事件选项以保证准确移除
    移除longpress监听器时，若没有绑定的事件则删除_longPressEvents

--- src/events.ts
+++ src/events.ts
@@ -1,7 +1,5 @@
-import type { Obj } from './types';
-
 /**
- * add custom longpress event for Node instance, default timeout is 500ms, which could be set by element's _longPressDelay /
+ * add custom longpress event for Node instance, default timeout is 500ms, which could be set by Node's _longPressDelay /
  * 为Node实例添加 longpress（长按）事件，触发超时500ms，可通过元素的 _longPressDelay 进行设置
  *
  * @example document.addEventListener('longpress', e => console.log(e))
@@ -14,36 +12,37 @@ Node.prototype.addEventListener = function (type, listener, options) {
     return;
   }
   if (!this.hasOwnProperty('_longPressEvents')) {
-    (this as Obj)._longPressEvents = [];
+    this._longPressEvents = [];
   }
-  let flag: true | null;
+  // 若绑定过同选项的longpress则直接跳过
+  if (this._longPressEvents!.some((e) => e.longpress === listener && e.options === options)) return;
+  let tid: number | null;
   const evt = {
     touchstart(e: Event) {
-      flag = true;
-      setTimeout(() => {
-        flag &&
-          listener.call(
-            this,
-            new CustomEvent('longpress', {
-              bubbles: e.bubbles,
-              cancelable: e.cancelable,
-              composed: e.composed,
-              ...(this as Obj)._longPressOption,
-              detail: e
-            })
-          );
-        flag = null;
-      }, (this as Obj)._longPressDelay || 500);
+      tid = setTimeout(() => {
+        listener.call(
+          this,
+          new CustomEvent('longpress', {
+            bubbles: e.bubbles,
+            cancelable: e.cancelable,
+            composed: e.composed,
+            ...(<Node>(<unknown>this))._longPressOption,
+            detail: e
+          })
+        );
+      }, (<Node>(<unknown>this))._longPressDelay || 500) as any;
     },
     touchend() {
-      flag = null;
+      clearTimeout(tid!);
+      tid = null;
     },
-    longpress: listener
+    longpress: listener,
+    options
   };
-  (this as Obj)._longPressEvents.push(evt);
+  this._longPressEvents!.push(evt);
   addListener.call(this, 'touchstart', evt.touchstart, options);
-  addListener.call(this, type, listener, options); // to be able to be called by dispatchEvent
-  addListener.call(this, 'touchend', evt.touchend);
+  addListener.call(this, type, listener, options); // able to be called by dispatchEvent
+  addListener.call(this, 'touchend', evt.touchend, options);
 };
 
 /**
@@ -58,12 +57,11 @@ Node.prototype.removeEventListener = function (type, listener, options) {
     removeListener.call(this, type, listener, options);
     return;
   }
-  const index: number = (this as Obj)._longPressEvents?.findIndex(
-    (e: { longpress: EventListenerOrEventListenerObject }) => e.longpress === listener
-  );
+  const index = this._longPressEvents?.findIndex((e) => e.longpress === listener && e.options === options);
   if (index === undefined || index < 0) return;
-  removeListener.call(this, 'touchstart', (this as Obj)._longPressEvents[index].touchstart, options);
-  removeListener.call(this, 'touchend', (this as Obj)._longPressEvents[index].touchend);
+  removeListener.call(this, 'touchstart', this._longPressEvents![index].touchstart, options);
+  removeListener.call(this, 'touchend', this._longPressEvents![index].touchend, options);
   removeListener.call(this, type, listener, options);
-  (this as Obj)._longPressEvents.splice(index, 1);
+  this._longPressEvents!.splice(index, 1);
+  if (!this._longPressEvents!.length) delete this._longPressEvents;
 };
