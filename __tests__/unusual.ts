5ab758c52410504b548ffb272e6538d8b33bb692
Xli33 <lshin1232@gmail.com>
2025-11-07 10:00:52 +0800

    test: 调整genUID测试用例

--- __tests__/unusual.ts
+++ __tests__/unusual.ts
@@ -54,8 +54,7 @@ describe('unusual modules', () => {
     expect(delArrItemByVal([2, '', fetch, console, false, NaN], ['', fetch, console, NaN])).toEqual([2, false]);
   });
   test('genUID', () => {
-    expect(+genUID(null, 1)).toBeCloseTo(0, 0);
-    expect(+genUID(null, 1)).toBeCloseTo(1, 0);
+    expect(+genUID(null, 1)).toBeLessThanOrEqual(1); // 在 0 ~ 1 以内取随机数一定小于 1
     expect(+genUID(null, 1000)).toBeLessThanOrEqual(1000);
     expect(genUID('Some')).toMatch(/^Some\d{1,3}$/);
   });
