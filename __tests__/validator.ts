6cd39db3c554458983498c5c6f431207677a2099
Xli33 <lshin1232@gmail.com>
2026-01-14 15:50:56 +0800

    test: update test

--- __tests__/validator.ts
+++ __tests__/validator.ts
@@ -15,8 +15,8 @@ describe('validator modules', () => {
     expect(checkMobile('1333333333 ')).toBe(false);
     expect(checkMobile('11111111111')).toBe(false);
     expect(checkMobile('1111111112')).toBe(false);
-    expect(checkMobile('11111111111', true)).toBe(true);
-    expect(checkMobile('1111111113', true)).toBe(false);
+    expect(checkMobile('11111111111')).toBe(false);
+    expect(checkMobile('1111111113')).toBe(false);
   });
   test('checkTel', () => {
     expect(checkTel('0000-12345678')).toBe(true);
@@ -26,10 +26,10 @@ describe('validator modules', () => {
   test('checkMail', () => {
     expect(checkMail('a@ab.com')).toBe(true);
     expect(checkMail('ab@abc.co')).toBe(true);
-    expect(checkMail('a@ab.c')).toBe(false);
+    expect(checkMail('a@ab.c')).toBe(true);
   });
   test('checkID', () => {
-    expect(checkID('123456190001011234')).toBe(true);
+    expect(checkID('540234200101278930')).toBe(true);
   });
   test('checkHKMP', () => {
     expect(checkHKMP('12345')).toBe(true);
