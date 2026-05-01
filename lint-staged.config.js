5d0c1b07344460ae1d0e2849a9d5afbc2932c3cf
Xli33 <lshin1232@gmail.com>
2026-05-01 23:52:09 +0800

    chore: 优化lint配置

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,16 +1,20 @@
 import { relative } from 'path';
 
+const getRelativePath = (filenames) => filenames.map((e) => relative(import.meta.dirname, e).replace(/\\/g, '/'));
+
 export default {
   '*.{js,ts}': (filenames) => {
     // 过滤要忽略的路径（例如 public 目录）
-    const filesToLint = filenames
-      .map((e) => relative(process.cwd(), e).replace(/\\/g, '/'))
-      .filter((e) => !e.startsWith('public/'));
+    const filesToLint = getRelativePath(filenames).filter((e) => !e.startsWith('public/'));
 
     return filesToLint.length
       ? [`eslint --cache --cache-location node_modules/.cache/eslint/ ${filesToLint.join(' ')}`]
       : [];
   },
 
-  '*.{js,ts,json,md}': ['prettier --check --ignore-unknown --experimental-cli']
+  '*.{js,ts,json,md}': (filenames) => [
+    `prettier --check --ignore-unknown --experimental-cli ${getRelativePath(filenames)
+      .filter((e) => !/CHANGELOG.md|^docs\//.test(e))
+      .join(' ')}`
+  ]
 };
